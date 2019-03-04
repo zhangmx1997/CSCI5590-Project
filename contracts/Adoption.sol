919b095c50b53233ce99a02209d3fd52a6c3e40a
Mingxue Zhang <zhangmx1997@foxmail.com>
2019-03-04 22:56:49 +0800

    add return function

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -10,9 +10,21 @@ contract Adoption {
         return petId;
     }
 
+    // return borrowed books
+    function giveback(uint petId) public returns (uint) {
+        
+        //emit Adopt(msg.sender, petId, adopters[petId]);
+
+        require(petId >= 0 && petId <= 15);  // ensure id does NOT exceed boundary
+        require(adopters[petId] == msg.sender); // only the user who borrowed this book and return it
+        
+        // clear record 
+        adopters[petId] = address(0x0000000000000000000000000000000000000000); 
+        return petId;
+    }
+
     // 返回领养者
     function getAdopters() public view returns (address[16] memory) {
         return adopters;
     }
-
 }
